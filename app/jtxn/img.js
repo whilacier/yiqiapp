3dcffa9c367b0cee6e5f1be7ea4b8f769a5f68ba
whilacier <whilacier@qq.com>
2025-09-11 08:24:08 +0800

    Add files via upload

---
+++ app/jtxn/img.js
@@ -0,0 +1,16 @@
+﻿//获取指定区间范围随机数，包括lowerValue和upperValue
+function randomFrom(lowerValue, upperValue) {
+	return Math.floor(Math.random() * (upperValue - lowerValue + 1) + lowerValue);
+}
+
+function getRndimg(){
+	var irndn=randomFrom(1,58)+".jpg";
+	//var isrc="http://yitixin.cn/app/jtxn/"+irndn;
+	return irndn;
+}
+
+// document.write(
+
+// '<p><img data-preview-src="" data-preview-group="1" src="'+isrc+'"/></p>'
+
+// )
